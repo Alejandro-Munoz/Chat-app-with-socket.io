a50d915b6db632a1eaf7a152a44f0c2bd3f4fe34
Alejandro <alejandro.z.munoz@gmail.com>
2015-10-15 10:30:16 -0700

    User id feature added

--- index.js
+++ index.js
@@ -11,19 +11,28 @@ app.get('/', function (req,res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+function User(){
+    var id=0;
+    this.getUser=function(){
+        return 'User '+ id++;
+    }
+}
+var u = new User();
 io.on('connection',function(socket){
+    var user = u.getUser();
+    console.log(user + ' connected');
+
     socket.on('chat message',function(msg){
-        io.emit('chat message',msg);
+        io.emit('chat message',user+' says: '+msg);
     });
-    console.log('a user connected');
-    io.on('disconnect',function(){
-        io.emit('disconnect','user disconnected')
+    socket.on('disconnect',function(){
+        io.emit('disconnect',user + ' disconnected')
 
     });
 });
 
 
-http.listen(3000, function () {
-    console.log('listening on port 3000');
+http.listen(8888, function () {
+    console.log('listening on port 8888');
 });
 
